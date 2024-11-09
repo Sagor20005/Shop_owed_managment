506e6c5c845913d697e069aa00955b3750b23245
Md Jakareya <109013040+Sagor20005@users.noreply.github.com>
2024-11-09 12:07:38 +0600

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -33,6 +33,10 @@ App.use(Express.static("./public"))
 require("./database/connect.js")
 // set default admin
 defaultAdmin()
+App.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      next();
+});
 
 // Routers
 App.use("/owed",owedRouter)
